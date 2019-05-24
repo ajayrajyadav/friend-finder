62a2d6c6a8482362d5e8a77c01dc9e0995e878e5
Ajay Yadav <ajay.yadav@AWKSOSX014.local>
2019-05-23 17:44:50 -0700

    Jquery populating questions works

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -16,29 +16,55 @@ $(document).ready(function () {
         $("#name-panel").append($("<input>").addClass("validate").attr({"id": "name", validate: "true", required: "true", type: "text"}));
         $("#name-panel").append($("<label>").attr("for","name").html("Your Name"));
 
-        $("#the-form").append($("<div>").addClass("row").attr("id","photo-row"));
+        $("#name-row").append($("<div>").addClass("row").attr("id","photo-row"));
         $("#photo-row").append($("<div>").addClass("input-field col s6").attr("id","photo-panel"));
         $("#photo-panel").append($("<input>").addClass("validate").attr({"id": "photo", validate: "true", required: "true", type: "text", placeholder: "http://"}));
         $("#photo-panel").append($("<label>").attr("for","name").html("Link to Photo Image (Required)"));
 
 
- 
-        
+        $("#the-form").append($("<div>").addClass("row").attr("id","survey-contianer"));
+        $("#survey-contianer").append($("<h4>").html("Question 1"));
+        $("#survey-contianer").append($("<div>").addClass("input-field col s12").attr("id","survey-row"));
 
+        $("#survey-row").append($("<select>").attr("id","question-1"));
+        $("#question-1").append($("<option>").attr({"value": "", disabled: "true", selected: "true"}).html("choose your option"));
+        $("#question-1").append($("<option>").attr("value", "1").html("Strongly Agree"));
+        $("#question-1").append($("<option>").attr("value", "2").html("Somewhat Agree"));
+        $("#question-1").append($("<option>").attr("value", "3").html("Neutral"));
+        $("#question-1").append($("<option>").attr("value", "4").html("Somewhat Disagree"));
+        $("#question-1").append($("<option>").attr("value", "5").html("Strongly Disagree"));
+        $("#survey-row").append($("<label>").html("I see myself as extroverted, enthusiastic."));
+        $('select').formSelect();
 
+        var questionFormContainer = $("#survey-contianer");
 
-        // <div class='card-panel yellow darken-1 col s12 center-align'>
-        // <h4><strong>Survey Questions</strong></h4>
-        // <div class='card-panel left-align white'>
-        //     <h5><strong>About You</strong></h5>
-        //     <br>
-        //     <input id="name" type="text" class="validate" required>
-        //     <label class='active' for="name">Name (required)</label>
-        //     <input id="photo" type="text" class="validate" required>
-        //     <label class='active' for="name">Link to Photo Image (required)</label>
-        //     <h5><strong>Question 1</strong></h5>
-        //     <p>I see myself as extroverted, enthusiastic.</p>
+        buildQuestions(questionFormContainer, 2, "some New survey question");
+
    
+    }
+
+    function buildQuestions(questionForm, questionNumber, questionString){
+
+        questionForm.append($("<h4>").html("Question "+questionNumber));
+        questionForm.append($("<div>").addClass("input-field col s12").attr("id","survey-row"+questionNumber));
+
+
+        $("#survey-row"+questionNumber).append($("<select>").attr("id","question-"+questionNumber));
+        $("#question-"+questionNumber).append($("<option>").attr({"value": "", disabled: "true", selected: "true"}).html("choose your option"));
+        $("#question-"+questionNumber).append($("<option>").attr("value", "1").html("Strongly Agree"));
+        $("#question-"+questionNumber).append($("<option>").attr("value", "2").html("Somewhat Agree"));
+        $("#question-"+questionNumber).append($("<option>").attr("value", "3").html("Neutral"));
+        $("#question-"+questionNumber).append($("<option>").attr("value", "4").html("Somewhat Disagree"));
+        $("#question-"+questionNumber).append($("<option>").attr("value", "5").html("Strongly Disagree"));
+        $("#survey-row"+questionNumber).append($("<label>").html(questionString));
+        $('select').formSelect();
+
+        // $("#survey-row"+questionNumber).append($("<select>").attr("id","question-"+questionNumber));
+        // $("#question-"+questionNumber).append($("<option>").attr({"value": "1", text: "Option 1"}));
+        // $("#question-"+questionNumber).append($("<option>").attr({"value": "2", text: "Option 2"}));
+        // $("#question-"+questionNumber).append($("<option>").attr("value", "3").html("option 3"));
+        // $("#survey-row"+questionNumber).append($("<label>").html(questionString));
+        // $('select').formSelect();
     }
         
 });
