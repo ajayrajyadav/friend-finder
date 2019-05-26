380f2abb9f83e5d6856198cfb9f6f55580688d45
ajayraj <ajayraj98@gmail.com>
2019-05-25 21:32:23 -0700

    final finishing touches

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@
 var friendList = [
     {
         "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://dramaguru.net/images/actors/ahmed_ali_akbar_191.jpeg?w=315&h=350&fit=crop-top",
         "scores": [
             5,
             1,
@@ -14,6 +14,22 @@ var friendList = [
             5,
             4,
             1
+        ]
+    },
+    {
+        "name": "Aaron",
+        "photo": "https://avatars3.githubusercontent.com/u/3124?s=460&v=4",
+        "scores": [
+            1,
+            1,
+            2,
+            4,
+            3,
+            1,
+            5,
+            2,
+            4,
+            4
         ]
     }
 ];
